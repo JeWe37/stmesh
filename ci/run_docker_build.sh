1cf33df1d0603f1567445f556e273ea3cf985a86
Jendrik Weise <jendrik.weise@rwth-aachen.de>
2024-05-23 22:47:03 +0000

    More efficient CI
    * Build artifacts now registered as such, rather than as cache
    * Images built with docker-container driver, to be able to cache build
      steps in GitLab CI cache
    * Image build only happens when Dockerfile changes, once at start of CI
      pipeline

--- ci/run_docker_build.sh
+++ ci/run_docker_build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-docker build -t stmesh-builder --build-arg='VARIANT=noble' --build-arg='GCC_VER=14' --build-arg='LLVM_VER=18' .devcontainer
 case $1 in
     build)
         docker run --user $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/repo -w /repo stmesh-builder /repo/ci/run_build.sh $2
