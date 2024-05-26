9e6e5a609d3ed878cb67eab71f7a8281190142e5
Jendrik Weise <jendrik.weise@rwth-aachen.de>
2024-05-26 18:56:37 +0000

    More rigorous testing for meshing algorithm
    * Actually checks all rules now
    * Rule 6 now tested more precisely
    * Parallelize tests and increase timeout

--- ci/run_docker_build.sh
+++ ci/run_docker_build.sh
@@ -4,6 +4,6 @@ case $1 in
         docker run --user $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/repo -w /repo stmesh-builder /repo/ci/run_build.sh $2
         ;;
     test)
-        docker run --user $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/repo -w /repo stmesh-builder ctest --preset test-$2
+        docker run --user $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/repo -w /repo stmesh-builder ctest --preset test-$2 -j$(nproc)
         ;;
 esac
